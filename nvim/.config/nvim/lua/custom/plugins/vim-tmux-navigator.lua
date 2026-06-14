0e8448e63e70dbe9644337fdc32b683c3004ac15
alanvillagrand <alan_villagrand1@baylor.edu>
2026-06-13 19:17:26 -0500

    Added toggleterm to nvim and modified vim-tmux navigation

--- nvim/.config/nvim/lua/custom/plugins/vim-tmux-navigator.lua
+++ nvim/.config/nvim/lua/custom/plugins/vim-tmux-navigator.lua
@@ -1,4 +1,13 @@
 -- vim-tmux-navigator
 -- https://github.com/christoomey/vim-tmux-navigator
 
+-- Disable default mappings to prevent <C-\> from being assigned
+vim.g.tmux_navigator_no_mappings = 1
+
 vim.pack.add { 'https://github.com/christoomey/vim-tmux-navigator' }
+
+vim.keymap.set("n", "<C-h>", "<cmd>TmuxNavigateLeft<CR>")
+vim.keymap.set("n", "<C-j>", "<cmd>TmuxNavigateDown<CR>")
+vim.keymap.set("n", "<C-k>", "<cmd>TmuxNavigateUp<CR>")
+vim.keymap.set("n", "<C-l>", "<cmd>TmuxNavigateRight<CR>")
+
